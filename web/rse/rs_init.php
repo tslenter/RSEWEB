8eb01cc13b74eb8011625ef667a1c6fd2aada9bd
Tom Slenter <slenter.tom@gmail.com>
2023-02-15 01:57:55 +0100

    Update rs_init.php

--- web/rse/rs_init.php
+++ web/rse/rs_init.php
@@ -32,6 +32,6 @@ if (isset($_SESSION['id']) && $_SESSION['un'] == true) {
 }
 
 require_once '/opt/RSEVIEW/vendor/autoload.php';
-use Elasticsearch\ClientBuilder;
-$es = ClientBuilder::create()->setHosts(['127.0.0.1:9200'])->build();
+//use Elasticsearch\ClientBuilder;
+$es = Elastic\Elasticsearch\ClientBuilder::create()->setHosts(['127.0.0.1:9200'])->build();
 ?>
